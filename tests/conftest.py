854fdaf2e5c16533dee2f88c7a7a1070f3a1d054
Cody Fincher <cody.fincher@gmail.com>
2024-04-09 17:07:52 +0000

    chore: updated tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,6 @@ if TYPE_CHECKING:
     from collections.abc import AsyncGenerator
 
 
-
 pytestmark = pytest.mark.anyio
 pytest_plugins = [
     "pytest_databases.docker",
@@ -21,7 +20,8 @@ pytest_plugins = [
 def anyio_backend() -> str:
     return "asyncio"
 
-@pytest.fixture(name="redis", autouse=True)
+
+@pytest.fixture(name="redis")
 async def fx_redis(docker_ip: str, redis_service: None, redis_port: int) -> AsyncGenerator[Redis, None]:
     """Redis instance for testing.
 
