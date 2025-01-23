61ded5b9729320427fd2ae3b6dd2985c85d9f802
Cody Fincher <cody@litestar.dev>
2025-01-23 22:32:02 +0000

    fix: remove flag

--- litestar_saq/plugin.py
+++ litestar_saq/plugin.py
@@ -68,12 +68,7 @@ class SAQPlugin(InitPluginProtocol, CLIPlugin):
         from litestar_saq.controllers import build_controller
 
         app_config.dependencies.update(
-            {
-                self._config.queues_dependency_key: Provide(
-                    dependency=self._config.provide_queues,
-                    sync_to_thread=False,
-                ),
-            },
+            {self._config.queues_dependency_key: Provide(dependency=self._config.provide_queues)}
         )
         if self._config.web_enabled:
             app_config.route_handlers.append(
