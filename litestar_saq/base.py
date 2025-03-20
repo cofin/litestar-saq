eaca99dbfac07bf2edbdbd8afe0f1e3a4807c115
Cody Fincher <cody@litestar.dev>
2025-03-20 18:14:38 +0000

    fix: add an ignore

--- litestar_saq/base.py
+++ litestar_saq/base.py
@@ -23,7 +23,7 @@ class Job(SaqJob):
 class CronJob(SaqCronJob):
     """Cron Job Details"""
 
-    function: "Union[Function, str]"
+    function: "Union[Function, str]"  # type: ignore[assignment]
     meta: "dict[str, Any]" = field(default_factory=dict)
 
     def __post_init__(self) -> None:
