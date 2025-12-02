fdddf14c4540807d80910d01b027f5027d773f99
Vikash <master.bvik@gmail.com>
2025-12-01 22:54:56 -0500

    feat: Enabled SAQ parameters for cancellation cleanup (#60)

--- litestar_saq/base.py
+++ litestar_saq/base.py
@@ -59,6 +59,8 @@ class Worker(SaqWorker):
         dequeue_timeout: float = 0,
         burst: bool = False,
         max_burst_jobs: "Optional[int]" = None,
+        shutdown_grace_period_s: "Optional[int]" = None,
+        cancellation_hard_deadline_s: float = 1.0,
         metadata: "Optional[JsonDict]" = None,
         separate_process: bool = True,
         multiprocessing_mode: Literal["multiprocessing", "threading"] = "multiprocessing",
@@ -80,6 +82,8 @@ class Worker(SaqWorker):
             dequeue_timeout=dequeue_timeout,
             burst=burst,
             max_burst_jobs=max_burst_jobs,
+            shutdown_grace_period_s=shutdown_grace_period_s,
+            cancellation_hard_deadline_s=cancellation_hard_deadline_s,
             metadata=metadata,
         )
 
