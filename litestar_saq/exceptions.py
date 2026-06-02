ee2b4cbb3ceadd7ba22c1989f90f25662053302c
Cody Fincher <cody@litestar.dev>
2026-06-02 18:21:52 +0000

    feat: align with latest SAQ features and clean up dead code

--- litestar_saq/exceptions.py
+++ litestar_saq/exceptions.py
@@ -7,7 +7,3 @@ class ImproperConfigurationError(LitestarSaqError):
 
     This exception is raised only when a module depends on a dependency that has not been installed.
     """
-
-
-class BackgroundTaskError(Exception):
-    """Base class for `Task` related exceptions."""
